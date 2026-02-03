7f4e7f7d2fb955a8badfaa4e9f9f96c98135ecdb
RYAN RIVERA <rrivera@FNAL.GOV>
2026-02-03 15:05:42 -0600

    trace severity and clangify

--- otsdaq-mu2e-trigger/TablePlugins/TriggerConfigTable.h
+++ otsdaq-mu2e-trigger/TablePlugins/TriggerConfigTable.h
@@ -1,31 +1,30 @@
 #ifndef _ots_TriggerConfigTable_h_
 #define _ots_TriggerConfigTable_h_
 
-#include "otsdaq/TableCore/TableBase.h"
-#include "otsdaq/ConfigurationInterface/ConfigurationManager.h"
-#include <string>
+#include <fstream>  // std::fstream
 #include <iostream>
-#include <fstream>      // std::fstream
+#include <string>
+#include "otsdaq/ConfigurationInterface/ConfigurationManager.h"
+#include "otsdaq/TableCore/TableBase.h"
 
 namespace ots
 {
 
-  class TriggerConfigTable : public TableBase
+// clang-format off
+class TriggerConfigTable : public TableBase
 {
-
-public:
-
+  public:
 	TriggerConfigTable(void);
 	virtual ~TriggerConfigTable(void);
 
 	//Methods
-	void   init                          (ConfigurationManager *configManager);
-	void    createTriggerMenuFiles        (std::ofstream& EpilogFclFile, std::string& EpilogDir, std::string& TrigPath, ots::ConfigurationTree  ConfTree);	
-	std::string GetModuleNameFromPath    (std::string &TrigPath);
-
-
-private:
-	bool 	isFirstAppInContext_;
+	void        init						(ConfigurationManager* configManager);
+	void        createTriggerMenuFiles		(std::ofstream&         EpilogFclFile,
+	                                  		 std::string&           EpilogDir,
+	                                  		 std::string&           TrigPath,
+	                                  		 ots::ConfigurationTree ConfTree);
+	std::string GetModuleNameFromPath		(std::string& TrigPath);
 };
-}
+// clang-format on
+}  // namespace ots
 #endif
